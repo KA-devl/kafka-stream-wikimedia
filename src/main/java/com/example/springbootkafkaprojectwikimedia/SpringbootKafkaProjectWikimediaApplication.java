ac724170465f025ec22d5d04d5fb30a2a562c8f4
KA-DEVL <anes1999@live.ca>
2023-08-31 10:28:21 -0700

    Setup multi module maven & spring boot project

---
+++ src/main/java/com/example/springbootkafkaprojectwikimedia/SpringbootKafkaProjectWikimediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkaprojectwikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaProjectWikimediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaProjectWikimediaApplication.class, args);
+	}
+
+}
