072ad23a0304b3e6b4acbd5388e161f58899a421
KA-DEVL <anes1999@live.ca>
2023-09-04 19:59:50 -0700

    Consuming real-time data with consumer

---
+++ kafka-consumer/src/main/java/com/example/consumer/KafkaConsumer.java
@@ -0,0 +1,17 @@
+package com.example.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+    @KafkaListener(topics = "wikimedia-topic", groupId="group_id")
+    public void consume(String message) {
+
+        LOGGER.info("Consumed new data! -> " +message);
+    }
+}
